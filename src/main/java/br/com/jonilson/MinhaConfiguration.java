edb826cf592f4da577295eeac20c5f6c44e4feae
Jonilson da Silva Leles <jsleles@gmail.com>
2024-05-11 15:37:09 -0300

    commitando versão profils

---
+++ src/main/java/br/com/jonilson/MinhaConfiguration.java
@@ -0,0 +1,21 @@
+package br.com.jonilson;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("development")
+public class MinhaConfiguration {
+
+	@Bean
+	public CommandLineRunner executar () {
+		return args -> {
+			
+			System.out.println("RODANDO A CONFIGURACAO DE DESENVOLVIMENTO");
+		};
+		
+	}
+
+}
